79fe94e5f900a8ec7484da1e2c148224b857e6d5
Matheus Junior <bonecodoido339@gmail.com>
2025-04-10 17:28:31 -0300

    style: Add estilo pagina de eventos

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EventoView from '@/views/EventoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/teste',
+      name: 'teste',
+      component: EventoView,
     }
     // {
     //   path: '/about',
