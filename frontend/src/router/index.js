526f75e7d39649fed2411099eb9f4d07e3869d65
Matheus Junior <bonecodoido339@gmail.com>
2025-05-08 16:26:15 -0300

    feat: Novas Paginas adicionadas falta separar por componente

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EventoView from '@/views/EventoView.vue'
+import AdminView from '@/views/AdminView.vue'
+import UsuarioView from '@/views/UsuarioView.vue'
+import EventosListView from '@/views/EventosListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +17,23 @@ const router = createRouter({
       path: '/teste',
       name: 'teste',
       component: EventoView,
+    },
+    {
+      path: '/teste2',
+      name: 'teste2',
+      component: AdminView
+    },
+    {
+      path: '/teste3',
+      name: 'teste3',
+      component: UsuarioView
+    },
+    {
+      path:'/teste4',
+      name: 'teste4',
+      component: EventosListView
     }
+
     // {
     //   path: '/about',
     //   name: 'about',
