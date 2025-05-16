17b68f88f15b8cc3b1eec5f4078344629ea80cd9
Pedro <pedromiguelboce@gmail.com>
2025-05-15 21:53:48 -0300

    refactor: ajustar rotas e remover caminhos de teste no roteador

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,48 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import EventoView from '@/views/EventoView.vue'
-import AdminView from '@/views/AdminView.vue'
-import UsuarioView from '@/views/UsuarioView.vue'
-import EventosListView from '@/views/EventosListView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import EventoView from "@/views/EventoView.vue";
+import AdminView from "@/views/AdminView.vue";
+import UsuarioView from "@/views/UsuarioView.vue";
+import EventosListView from "@/views/EventosListView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/teste',
-      name: 'teste',
+      path: "/evento",
+      name: "evento",
       component: EventoView,
     },
     {
-      path: '/teste2',
-      name: 'teste2',
-      component: AdminView
+      path: "/dashboard",
+      name: "dashboard",
+      component: AdminView,
     },
     {
-      path: '/teste3',
-      name: 'teste3',
-      component: UsuarioView
+      path: "/perfil",
+      name: "perfil",
+      component: UsuarioView,
     },
     {
-      path:'/teste4',
-      name: 'teste4',
-      component: EventosListView
-    }
-
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+      path: "/eventos",
+      name: "eventos",
+      component: EventosListView,
+    },
   ],
-})
+});
 
-export default router
+export default router;
