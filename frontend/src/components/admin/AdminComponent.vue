1dd53c7b55b8835fab090de9ccf6b731a2aecf8e
Matheus Junior <bonecodoido339@gmail.com>
2025-11-22 21:46:05 -0300

    fix: Imagem Quebrada na pagina area de organizador

--- frontend/src/components/admin/AdminComponent.vue
+++ frontend/src/components/admin/AdminComponent.vue
@@ -4,8 +4,10 @@
       <div class="sidebar" :class="{ 'sidebar-collapsed': sidebarCollapsed }">
         <div class="sidebar-header">
           <div class="logo-container">
-            <img v-if="!sidebarCollapsed" src="../../assets/LogoConecta.png" alt="Conecta Eventos Logo" height="100" class="logo">
-            <img v-else>
+            <router-link to="/">
+              <img v-if="!sidebarCollapsed" src="../../assets/logo.svg" alt="Conecta Eventos Logo" height="100" class="logo">
+              <img v-else>
+            </router-link>
           </div>
           <button class="btn btn-sm btn-link text-primary sidebar-toggle" @click="toggleSidebar">
             <i class="bi" :class="sidebarCollapsed ? 'bi-chevron-right' : 'bi-chevron-left'"></i>
